c451c0f04881c74d8c23086ba5c3b1f13dd82ea3
Xandyhoss <xandyharrison@gmail.com>
2022-08-29 11:32:33 -0300

    Added cards and animations w/ framer-motion

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 /* eslint-disable @next/next/no-img-element */
 const sizes = ["large", "medium", "small"] as const;
@@ -8,10 +9,7 @@ type PropsType = {
 };
 
 export default function Card(props: PropsType) {
-  const {
-    imgUrl = "https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg",
-    size = "medium",
-  } = props;
+  const { imgUrl = "/img/card-img.jpg", size = "medium" } = props;
   const classMap = {
     large: "w-[250px] h-[350px]",
     medium: "w-[180px] h-[280px]",
@@ -19,12 +17,14 @@ export default function Card(props: PropsType) {
   };
 
   const imgErrorHandler = () => {
-    console.log('Error on image, ID: ')
+    console.log("Error on image, ID: ");
   };
-
   return (
-    <div className={`${classMap[size]} relative cursor-pointer`}>
-      <p className="z-[1]">card size: {size}</p>
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.98 }}
+      className={`${classMap[size]} relative cursor-pointer overflow-hidden rounded-md z-[1] hover:z-20 hover:drop-shadow-md`}
+    >
       <Image
         src={imgUrl}
         alt="Image"
@@ -32,6 +32,6 @@ export default function Card(props: PropsType) {
         className="object-cover z-[-1] object-center mx-auto my-auto block absolute"
         onError={imgErrorHandler}
       />
-    </div>
+    </motion.div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,22 @@ import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
+const data: { id: number; imgUrl?: string }[] = [];
+
+for (var i = 0; i <= 9; i++) {
+  if (i % 2 == 0) {
+    data.push({
+      id: i,
+      imgUrl:
+        "https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg",
+    });
+  } else {
+    data.push({
+      id: i,
+    });
+  }
+}
+
 const Home: NextPage = () => {
   return (
     <div className="h-screen">
@@ -19,18 +35,16 @@ const Home: NextPage = () => {
         imgUrl="https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg"
       />
       <div className="w-full p-10 gap-y-10 flex flex-col">
-        <Card
-          imgUrl="https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg"
-          size="large"
-        />
-        <Card
-          imgUrl="https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg"
-          size="medium"
-        />
-        <Card
-          imgUrl="https://images.wallpaperscraft.com/image/single/toyota_supra_green_front_view_102266_1920x1080.jpg"
-          size="small"
-        />
+        <div className="flex gap-5">
+          {data.map((item, i) => {
+            return <Card imgUrl={item.imgUrl} key={i} />;
+          })}
+        </div>
+        <div className="flex gap-5">
+          {data.map((item, i) => {
+            return <Card imgUrl={item.imgUrl} key={i} />;
+          })}
+        </div>
       </div>
     </div>
   );
